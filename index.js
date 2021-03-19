747bd477edbb5cfc5cd9d2caab7eaf03a19318d5
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-03-19 12:22:47 -0300

    adicionando o mongodb no projeto e criando a conexao com o banco de dados // escolhi o mongo para este teste, pois ele e' facil de implantar, rapido, nao existem relacionamentos, pois so existe uma unica tabela, entao, creio que atende bem

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://renatoteste:renatoteste@cluster0.zuoer.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,  
+  useUnifiedTopology: true,
+})
+
+app.use(express.json())
 
 app.get('/', (request, response) => {
   return response.send({message: 'Hello World'})
